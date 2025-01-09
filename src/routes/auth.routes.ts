caba9fd2e5423faae5bc47b163f598e74681f6a0
Arash Nozarinejad <nozarinejad.arash@gmail.com>
2025-01-09 16:42:00 +0400

    validation added to authentication routes as well as the addition of /profile route to get profile information of a user based on token

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { registerUser, loginUser } from "../controllers/auth.controller";
+import { registerUser, loginUser, getProfile } from "../controllers/auth.controller";
+import { validate } from "../middlewares/validation.middleware";
+import { registerValidation, loginValidation } from "../validators/auth.validators";
+import { authenticateToken } from "../middlewares/auth.middleware";
 
 const router = Router();
 
-router.post('/register', registerUser);
-router.post('/login', loginUser)
+router.post('/register', validate(registerValidation),registerUser);
+router.post('/login', validate(loginValidation),loginUser);
+router.get('/profile', authenticateToken, getProfile);
 
 export default router;
