370ada5c879fd03abe65b29e51769ce7d9ec470e
Arash Nozarinejad <nozarinejad.arash@gmail.com>
2025-01-09 14:49:51 +0400

    routes added to the express server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import authRoutes from './routes/auth.routes';
+import postRoutes from './routes/post.routes';
 
 import { testConnection } from './utils/database';
 
@@ -10,6 +12,9 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/posts', postRoutes);
 
 async function startServer() {
     try {
