e61089f425dae0e9dfb81f5ee1f70c94c7e1ea55
Arash Nozarinejad <nozarinejad.arash@gmail.com>
2025-01-09 16:25:18 +0400

    token authentication middleware created

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,33 @@
+import { Response, NextFunction } from "express";
+import { AuthRequest } from "../types";
+import jwt from 'jsonwebtoken';
+
+export const authenticateToken = async (
+    req: AuthRequest,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        const authHeader = req.headers['authorization'];
+
+        const token = authHeader && authHeader.split('')[1];
+
+        if (!token){
+            res.status(401).json({ message: 'Authentication token required!' });
+            return;
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as any;
+
+        req.user = {
+            id: decoded.id,
+            email: decoded.email,
+            username: decoded.user
+        };
+
+        next();
+    } catch (error) {
+        res.status(403).json({ message: 'Invalid token' });
+        return;
+    }
+}
