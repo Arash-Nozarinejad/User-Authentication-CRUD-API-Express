3333c8c0fb29637375409543f955f49f4b818abb
Arash Nozarinejad <nozarinejad.arash@gmail.com>
2025-01-09 16:30:17 +0400

    auth validation for login and register created

---
+++ src/validators/auth.validators.ts
@@ -0,0 +1,12 @@
+import { body } from "express-validator";
+
+export const registerValidation = [
+    body('email').isEmail().withMessage('Please enter a valid email'),
+    body('username').isLength({ min: 3 }).withMessage('username must be at least 3 characters long'),
+    body('password').isLength({ min: 6 }).withMessage('password must be at least 6 characters long')
+];
+
+export const loginValidation = [
+    body('Email').withMessage('Please enter a valid email'),
+    body('password').exists().withMessage('password is required')
+];
