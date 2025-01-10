2939abb472c177fb8f2954588df7049b4232cea5
Arash Nozarinejad <nozarinejad.arash@gmail.com>
2025-01-10 11:14:59 +0400

    public and protected routes defined for posts

--- src/routes/post.routes.ts
+++ src/routes/post.routes.ts
@@ -1,5 +1,18 @@
 import { Router } from "express";
+import { authenticateToken } from "../middlewares/auth.middleware";
+import { validate } from "../middlewares/validation.middleware";
+import { createPostValidation, updatePostValidation } from "../validators/post.validators";
+import { createPost, updatePost, deletePost, getAllPosts, getUserPosts, getPost } from "../controllers/post.controller";
 
 const router = Router();
+
+router.get('/', getAllPosts);
+router.get('/:id', getPost);
+
+router.use(authenticateToken);
+router.post('/', validate(createPostValidation), createPost);
+router.get('/user/posts', getUserPosts);
+router.put('/:id', validate(updatePostValidation), updatePost);
+router.delete('/:id', deletePost);
 
 export default router;
